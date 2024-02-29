e9043386a1dff111dc903f787dcc298296accdde
YuBin <bin8727@naver.com>
2024-02-29 16:03:40 +0900

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by 모유빈 on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
